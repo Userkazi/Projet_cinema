d9fd9cfb58784fd5dc6b37abe72d6b90d448aaba
Userkazi <benjaminmbiki@gmail.com>
2026-05-05 12:18:52 -0400

    update db.js

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
-const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
-module.exports = pool;
+//const mysql = require("mysql2/promise");
+
+//const pool = mysql.createPool({
+    //host: process.env.DB_HOST,
+    //user: process.env.DB_USER,
+    //password: process.env.DB_PASSWORD,
+    //database: process.env.DB_NAME
+//});
+const mysql = require('mysql2');
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+
+module.exports = connection;
